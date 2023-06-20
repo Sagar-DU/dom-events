c95f73b5253b7062adee8ec5d444879812d2a102
Sagar-DU <nahidhasansagar@gmail.com>
2023-06-21 02:07:13 +0600

    event stuff is cool

--- js/script.js
+++ js/script.js
@@ -2,11 +2,27 @@ function makeGreen (){
     document.body.style.backgroundColor = "green";
 }
 
-//On-click even in js
+//On-click event in js
 const blueButton = document.getElementById("blue-btn");
 
 blueButton.onclick = makeBlue;
 
 function makeBlue(){
     document.body.style.backgroundColor = "blue";
+}
+
+//On-click event with anonymous function
+const whiteButton = document.getElementById("white-btn");
+
+whiteButton.onclick = function (){
+    document.body.style.backgroundColor = "white";
+}
+
+//Event listner in action
+const purpuleButton = document.getElementById("purple-btn");
+
+purpuleButton.addEventListener("click", makePurple);
+
+function makePurple (){
+    document.body.style.backgroundColor = "purple";
 }
