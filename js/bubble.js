5725dcff258dcfff9450c6a59480ce8fc6506341
Sagar-DU <nahidhasansagar@gmail.com>
2023-07-01 16:52:28 +0600

    Bubble effect practiced

--- js/bubble.js
+++ js/bubble.js
@@ -1,9 +1,26 @@
 // Li ar upor dewa hoise 
-document.getElementById("second-item").addEventListener("click", function(){
+document.getElementById("second-item").addEventListener("click", function(event){
     console.log("Second item paisiiiiiii!!");
+    // to stop bubble effect 
+    event.stopImmediatePropagation();
+})
+document.getElementById("second-item").addEventListener("click", function(event){
+    console.log("Second item paisiiiiiii!! Abar");
+    // to stop bubble effect 
+    // event.stopPropagation();
+})
+document.getElementById("second-item").addEventListener("click", function(event){
+    console.log("Second item paisiiiiiii!! Abar Abar");
+    // to stop bubble effect 
+    // event.stopPropagation();
 })
 
 // ul ar upor dewa hoise 
 document.getElementById("list-container").addEventListener("click", function(){
-    console.log("Lists paisi");
+    console.log("Lists paisi!");
+})
+
+//section ar upor dewa hoise
+document.getElementById("section-container").addEventListener("click", function(){
+    console.log("Section re click korlam.")
 })
