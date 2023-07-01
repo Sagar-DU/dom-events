b6ff1e4e368206d91eae49ef377c4f11fe1021da
Sagar-DU <nahidhasansagar@gmail.com>
2023-07-01 19:50:59 +0600

    delegate is cool!

--- js/delegate.js
+++ js/delegate.js
@@ -8,8 +8,22 @@ const items = document.getElementsByClassName("item");
 // }
 
 // Fun way 
-for (const item of items){
-    item.addEventListener("click", function(event){
-        event.target.parentNode.removeChild(event.target);
-    })
-}
+// for (const item of items){
+//     item.addEventListener("click", function(event){
+//         event.target.parentNode.removeChild(event.target);
+//     })
+// }
+
+//Adding new list item
+document.getElementById("add-item").addEventListener("click", function(){
+    const li = document.createElement("li");
+    li.classList.add("item");
+    li.innerText = "Notun Gunda!";
+    const parent = document.getElementById("item-container");
+    parent.appendChild(li);
+})
+
+// works for new item also 
+document.getElementById("item-container").addEventListener("click", function(event){
+    event.target.parentNode.removeChild(event.target);
+})
