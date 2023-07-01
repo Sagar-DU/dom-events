83d0297862e35017c630578fafa9f4c1de93dc8e
Sagar-DU <nahidhasansagar@gmail.com>
2023-07-01 16:42:31 +0600

    html modified

---
+++ js/bubble.js
@@ -0,0 +1,9 @@
+// Li ar upor dewa hoise 
+document.getElementById("second-item").addEventListener("click", function(){
+    console.log("Second item paisiiiiiii!!");
+})
+
+// ul ar upor dewa hoise 
+document.getElementById("list-container").addEventListener("click", function(){
+    console.log("Lists paisi");
+})
