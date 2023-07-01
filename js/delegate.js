05398fb52dfbe578d7d712463143444751377c35
Sagar-DU <nahidhasansagar@gmail.com>
2023-07-01 17:09:05 +0600

    Cool!

--- js/delegate.js
+++ js/delegate.js
@@ -1,7 +1,15 @@
 const items = document.getElementsByClassName("item");
 
+// Easy way 
+// for (const item of items){
+//     item.addEventListener("click", function(){
+//         document.getElementById("item-container").removeChild(item);
+//     })
+// }
+
+// Fun way 
 for (const item of items){
-    item.addEventListener("click", function(){
-        document.getElementById("item-container").removeChild(item);
+    item.addEventListener("click", function(event){
+        event.target.parentNode.removeChild(event.target);
     })
 }
